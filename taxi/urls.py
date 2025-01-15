b63d924689732e56f3b757ae4b381f17ce07991d
Oleksandr <rigby8806@gmail.com>
2025-01-15 21:14:36 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,7 +7,11 @@ from .views import (
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
+    toggle_car,
     DriverListView,
+    DriverCreateView,
+    DriverLicenseUpdateView,
+    DriverDeleteView,
     DriverDetailView,
     ManufacturerListView,
     ManufacturerCreateView,
@@ -45,6 +49,17 @@ urlpatterns = [
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete",
+    ),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverLicenseUpdateView.as_view(),
+        name="driver-update",
     ),
 ]
 
